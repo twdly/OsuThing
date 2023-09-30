326773ec1381eb4817dd9b7fb2ddbbeaebe38935
twdly <taiwoodley@gmail.com>
2023-09-30 22:25:27 +1000

    Add difficulty rating and mods to scores

--- OsuThing/Models/ScoreModel.cs
+++ OsuThing/Models/ScoreModel.cs
@@ -4,15 +4,25 @@ namespace OsuThing.Models;
 
 public class ScoreModel
 {
-    [JsonPropertyName("id")] public long? Id { get; set; }
-    [JsonPropertyName("accuracy")] public double? Accuracy { get; set; }
+    [JsonPropertyName("accuracy")] public double Accuracy { get; set; }
     [JsonPropertyName("pp")] public double? Pp { get; set; }
-    [JsonPropertyName("max_combo")] public int? MaxCombo { get; set; }
-    [JsonPropertyName("beatmap")] public BeatmapModel? Beatmap { get; set; }
-    [JsonPropertyName("beatmapset")] public BeatmapSetModel? BeatmapSet { get; set; }
+    [JsonPropertyName("max_combo")] public int MaxCombo { get; set; }
+    [JsonPropertyName("beatmap")] public required BeatmapModel Beatmap { get; set; }
+    [JsonPropertyName("beatmapset")] public required BeatmapSetModel BeatmapSet { get; set; }
+    [JsonPropertyName("mods")] public required List<string> Mods { get; set; }
 
     public double GetRoundedAccuracy()
     {
-        return Math.Round((double)Accuracy! * 100, 2);
+        return Math.Round(Accuracy * 100, 2);
+    }
+
+    public string GetModsAsString()
+    {
+        if (Mods.Count == 0)
+        {
+            return "NM";
+        }
+        const string modString = "+";
+        return Mods.Aggregate(modString, (current, mod) => current + mod);
     }
 }
