d126c1113dc901ceb9d9875c1c566daca5895410
twdly <taiwoodley@gmail.com>
2025-02-11 22:25:39 +1100

    Add accuracy and PP to the comparison and colour better values

--- OsuThing/Pages/ScoreComparisonDisplay.razor.cs
+++ OsuThing/Pages/ScoreComparisonDisplay.razor.cs
@@ -8,4 +8,11 @@ public partial class ScoreComparisonDisplay : ComponentBase
     [Parameter] public required ScoreModel Score { get; set; }
     [Parameter] public required ScoreModel ComparisonScore { get; set; }
     [Parameter] public required UserModel User { get; set; }
+
+    private static string GetComparisonClass(double score, double comparison, bool higherIsBetter)
+    {
+        var isBetter = score > comparison;
+        isBetter = higherIsBetter ? isBetter : !isBetter;
+        return isBetter ? "better" : "worse";
+    }
 }
