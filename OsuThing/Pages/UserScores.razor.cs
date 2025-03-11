ed7b1eb7b77965737a036aa374b6ef4f43fa6bba
twdly <taiwoodley@gmail.com>
2025-03-11 23:21:35 +1100

    Implement debounce for user scores user input

--- OsuThing/Pages/UserScores.razor.cs
+++ OsuThing/Pages/UserScores.razor.cs
@@ -1,11 +1,13 @@
+using System.Timers;
 using Microsoft.AspNetCore.Components;
 using OsuThing.Enums;
 using OsuThing.Models;
 using OsuThing.Services.Interfaces;
+using Timer = System.Timers.Timer;
 
 namespace OsuThing.Pages;
 
-public partial class UserScores
+public partial class UserScores : IDisposable
 {
     [Inject] private IScoreService ScoreService { get; set; } = null!;
     [Inject] private IUserService UserService { get; set; } = null!;
@@ -15,6 +17,9 @@ public partial class UserScores
     private string _scoresMessage = "";
     private UserScoreType _scoreType = UserScoreType.Best; // Default type is best as most players care primarily about the PP value of their score
     private int? _scoreCount = 100;
+
+    private Timer _debounceTimer = default!;
+    private string? _userInput = "";
 
     private async void GetScores()
     {
@@ -29,13 +34,16 @@ public partial class UserScores
         StateHasChanged();
     }
 
-    private async void FindUser(string? input)
+    private async void FindUser(object? sender, ElapsedEventArgs e)
     {
-        if (input != null)
-        {
-            _user = await UserService.FindUser(input);
-        }
-        StateHasChanged();
+        _user = await UserService.FindUser(_userInput);
+        await InvokeAsync(StateHasChanged);
+    }
+
+    private void DebounceFindUser()
+    {
+        _debounceTimer.Stop();
+        _debounceTimer.Start();
     }
 
     private bool IsScoreButtonDisabled()
@@ -53,5 +61,18 @@ public partial class UserScores
         var capital = stringToCapitalise[0];
         var remaining = stringToCapitalise[1..];
         return char.ToUpper(capital) + remaining;
+    }
+
+    protected override void OnInitialized()
+    {
+        _debounceTimer = new Timer(500);
+        _debounceTimer.Elapsed += FindUser;
+        _debounceTimer.AutoReset = false;
+    }
+
+    public void Dispose()
+    {
+        _debounceTimer.Dispose();
+        GC.SuppressFinalize(this);
     }
 }
