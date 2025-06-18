9eab585b845ab96e0f75818a97280fcb87573d56
Tai <taiwoodley@gmail.com>
2025-06-18 22:39:24 +1000

    Fix score comparison showing some stats as worse when comparing against an empty score

--- OsuThing/Pages/Components/ScoreComparisonDisplay.razor.cs
+++ OsuThing/Pages/Components/ScoreComparisonDisplay.razor.cs
@@ -9,8 +9,13 @@ public partial class ScoreComparisonDisplay : ComponentBase
     [Parameter] public required ScoreModel ComparisonScore { get; set; }
     [Parameter] public required UserModel User { get; set; }
 
-    private static string GetComparisonClass(double? score, double? comparison, bool higherIsBetter)
+    private string GetComparisonClass(double? score, double? comparison, bool higherIsBetter)
     {
+        if (Score.Score == 0 || ComparisonScore.Score == 0)
+        {
+            // Score has nothing to be compared to and, therefore, is inherently better
+            return "better";
+        }
         var isBetter = score > comparison;
         isBetter = higherIsBetter ? isBetter : !isBetter;
         return isBetter ? "better" : "worse";
